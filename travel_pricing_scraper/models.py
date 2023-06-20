0f4eda9003d7f266ae90640fd7ee132d2c86eb29
Kalel L. Martinho <kalel@tutamail.com>
2023-06-20 13:29:40 -0300

    test: doc tests updated to cover carrier

--- travel_pricing_scraper/models.py
+++ travel_pricing_scraper/models.py
@@ -6,8 +6,8 @@ from pydantic import BaseModel, validator
 
 class Flight(BaseModel):
     """Flight model.
-    >>> Flight(id=uuid.UUID('50b5e710-db51-424c-b052-8383635ad39d'), origin='CWB', destination='POA', departure_date=dt(2021, 7, 8, 0, 0), arrive_date=dt(2021, 7, 10, 0, 0), price=100.0)
-    Flight(id=UUID('50b5e710-db51-424c-b052-8383635ad39d'), origin='CWB', destination='POA', departure_date=datetime.datetime(2021, 7, 8, 0, 0), arrive_date=datetime.datetime(2021, 7, 10, 0, 0), price=100.0)
+    >>> Flight(id=uuid.UUID('50b5e710-db51-424c-b052-8383635ad39d'), origin='CWB', destination='POA', departure_date=dt(2021, 7, 8, 0, 0), arrive_date=dt(2021, 7, 10, 0, 0), price=100.0, carrier='GOL')
+    Flight(id=UUID('50b5e710-db51-424c-b052-8383635ad39d'), origin='CWB', destination='POA', departure_date=datetime.datetime(2021, 7, 8, 0, 0), arrive_date=datetime.datetime(2021, 7, 10, 0, 0), price=100.0, carrier='GOL')
     """
 
     id: uuid.UUID = uuid.uuid4()
@@ -21,12 +21,12 @@ class Flight(BaseModel):
     @validator('price')
     def price_must_be_positive(cls, v):
         """Validates that price is positive.
-        >>> Flight(id=uuid.UUID('50b5e710-db51-424c-b052-8383635ad39d'), origin='CWB', destination='POA', departure_date=dt(2021, 7, 8, 0, 0), arrive_date=dt(2021, 7, 10, 0, 0), price=-100.0)
+        >>> Flight(id=uuid.UUID('50b5e710-db51-424c-b052-8383635ad39d'), origin='CWB', destination='POA', departure_date=dt(2021, 7, 8, 0, 0), arrive_date=dt(2021, 7, 10, 0, 0), price=-100.0, carrier='GOL')
         Traceback (most recent call last):
           File "/usr/lib/python3.10/doctest.py", line 1350, in __run
             exec(compile(example.source, filename, "single",
           File "<doctest travel_pricing_scraper.models.Flight.price_must_be_positive[0]>", line 1, in <module>
-            Flight(id=uuid.UUID('50b5e710-db51-424c-b052-8383635ad39d'), origin='CWB', destination='POA', departure_date=dt(2021, 7, 8, 0, 0), arrive_date=dt(2021, 7, 10, 0, 0), price=-100.0)
+            Flight(id=uuid.UUID('50b5e710-db51-424c-b052-8383635ad39d'), origin='CWB', destination='POA', departure_date=dt(2021, 7, 8, 0, 0), arrive_date=dt(2021, 7, 10, 0, 0), price=-100.0, carrier='GOL')
           File "pydantic/main.py", line 341, in pydantic.main.BaseModel.__init__
         pydantic.error_wrappers.ValidationError: 1 validation error for Flight
         price
@@ -37,8 +37,8 @@ class Flight(BaseModel):
     @validator('departure_date', 'arrive_date')
     def datestring_to_datetime(cls, v):
         """Converts date string to datetime.
-        >>> Flight(id=uuid.UUID('50b5e710-db51-424c-b052-8383635ad39d'), origin='CWB', destination='POA', departure_date='2021-07-08', arrive_date='2021-07-10', price=100.0)
-        Flight(id=UUID('...'), origin='CWB', destination='POA', departure_date=datetime.datetime(2021, 7, 8, 0, 0), arrive_date=datetime.datetime(2021, 7, 10, 0, 0), price=100.0)"""
+        >>> Flight(id=uuid.UUID('50b5e710-db51-424c-b052-8383635ad39d'), origin='CWB', destination='POA', departure_date='2021-07-08', arrive_date='2021-07-10', price=100.0, carrier='GOL')
+        Flight(id=UUID('...'), origin='CWB', destination='POA', departure_date=datetime.datetime(2021, 7, 8, 0, 0), arrive_date=datetime.datetime(2021, 7, 10, 0, 0), price=100.0, carrier='GOL')"""
         if isinstance(v, str):
             return dt.fromisoformat(v)
         return v
